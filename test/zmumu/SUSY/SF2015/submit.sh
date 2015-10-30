73aa7a094b9ba5ea1562ef506b1f5a755fe2d184
gael perrin <gael.ludovic.perrin@cern.ch>
2015-10-30 11:19:34 +0100

    .

--- test/zmumu/SUSY/SF2015/submit.sh
+++ test/zmumu/SUSY/SF2015/submit.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-bsub -q 8nh -u pippo1234 run.sh test1 1 data_all 25ns 2015D
-bsub -q 8nh -u pippo1234 run.sh test1 2 data_all 25ns 2015D
-bsub -q 8nh -u pippo1234 run.sh test1 3 data_all 25ns 2015D
-bsub -q 8nh -u pippo1234 run.sh test1 4 data_all 25ns 2015D
-bsub -q 8nh -u pippo1234 run.sh test1 5 data_all 25ns 2015D
-bsub -q 8nh -u pippo1234 run.sh test1 6 data_all 25ns 2015D
-bsub -q 8nh -u pippo1234 run.sh test1 7 data_all 25ns 2015D
-bsub -q 8nh -u pippo1234 run.sh test1 8 data_all 25ns 2015D 
-bsub -q 8nh -u pippo1234 run.sh test1 9 data_all 25ns 2015D 
-bsub -q 8nh -u pippo1234 run.sh test1 10 data_all 25ns 2015D 
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 1 data_all 25ns 2015D
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 2 data_all 25ns 2015D
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 3 data_all 25ns 2015D
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 4 data_all 25ns 2015D
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 5 data_all 25ns 2015D
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 6 data_all 25ns 2015D
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 7 data_all 25ns 2015D
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 8 data_all 25ns 2015D 
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 9 data_all 25ns 2015D 
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 10 data_all 25ns 2015D 
 #
-bsub -q 8nh -u pippo1234 run.sh test1 1 mc_all 25ns 2015D NLO 
-bsub -q 8nh -u pippo1234 run.sh test1 2 mc_all 25ns 2015D NLO
-bsub -q 8nh -u pippo1234 run.sh test1 3 mc_all 25ns 2015D NLO
-bsub -q 8nh -u pippo1234 run.sh test1 4 mc_all 25ns 2015D NLO
-bsub -q 8nh -u pippo1234 run.sh test1 5 mc_all 25ns 2015D NLO
-bsub -q 8nh -u pippo1234 run.sh test1 6 mc_all 25ns 2015D NLO
-bsub -q 8nh -u pippo1234 run.sh test1 7 mc_all 25ns 2015D NLO
-bsub -q 8nh -u pippo1234 run.sh test1 8 mc_all 25ns 2015D NLO
-bsub -q 8nh -u pippo1234 run.sh test1 9 mc_all 25ns 2015D NLO
-bsub -q 8nh -u pippo1234 run.sh test1 10 mc_all 25ns 2015D NLO
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 1 mc_all 25ns 2015D NLO 
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 2 mc_all 25ns 2015D NLO
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 3 mc_all 25ns 2015D NLO
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 4 mc_all 25ns 2015D NLO
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 5 mc_all 25ns 2015D NLO
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 6 mc_all 25ns 2015D NLO
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 7 mc_all 25ns 2015D NLO
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 8 mc_all 25ns 2015D NLO
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 9 mc_all 25ns 2015D NLO
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 10 mc_all 25ns 2015D NLO
 #
-bsub -q 8nh -u pippo1234 run.sh test1 1 mc_all 25ns 2015D LO 
-bsub -q 8nh -u pippo1234 run.sh test1 2 mc_all 25ns 2015D LO
-bsub -q 8nh -u pippo1234 run.sh test1 3 mc_all 25ns 2015D LO
-bsub -q 8nh -u pippo1234 run.sh test1 4 mc_all 25ns 2015D LO
-bsub -q 8nh -u pippo1234 run.sh test1 5 mc_all 25ns 2015D LO
-bsub -q 8nh -u pippo1234 run.sh test1 6 mc_all 25ns 2015D LO
-bsub -q 8nh -u pippo1234 run.sh test1 7 mc_all 25ns 2015D LO
-bsub -q 8nh -u pippo1234 run.sh test1 8 mc_all 25ns 2015D LO
-bsub -q 8nh -u pippo1234 run.sh test1 9 mc_all 25ns 2015D LO
-bsub -q 8nh -u pippo1234 run.sh test1 10 mc_all 25ns 2015D LO
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 1 mc_all 25ns 2015D LO 
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 2 mc_all 25ns 2015D LO
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 3 mc_all 25ns 2015D LO
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 4 mc_all 25ns 2015D LO
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 5 mc_all 25ns 2015D LO
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 6 mc_all 25ns 2015D LO
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 7 mc_all 25ns 2015D LO
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 8 mc_all 25ns 2015D LO
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 9 mc_all 25ns 2015D LO
+bsub -q 8nh -u pippo1234 run.sh 5pt_9999relact 10 mc_all 25ns 2015D LO
 
