7f2a47cf22cee86969558af36f48686242e0cb66
Jan <hoss@phys.ethz.ch>
2015-10-30 10:54:32 +0100

    personalize submission scripts

--- test/zmumu/SUSY/SF2015/submit.sh
+++ test/zmumu/SUSY/SF2015/submit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#bsub -q 8nh -u pippo1234 run.sh data_all 25ns 2015D 1
+bsub -q 8nh -u pippo1234 run.sh data_all 25ns 2015D 1
 #bsub -q 8nh -u pippo1234 run.sh data_all 25ns 2015D 2
 #bsub -q 8nh -u pippo1234 run.sh data_all 25ns 2015D 3
 #bsub -q 8nh -u pippo1234 run.sh data_all 25ns 2015D 4
@@ -17,10 +17,10 @@
 #bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 4 NLO
 #bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 5 NLO
 #bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 6 NLO
-bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 7 NLO
-bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 8 NLO
-bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 9 NLO
-bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 10 NLO
+#bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 7 NLO
+#bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 8 NLO
+#bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 9 NLO
+#bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 10 NLO
 #
 #bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 1 LO 
 #bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 2 LO
@@ -28,8 +28,8 @@ bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 10 NLO
 #bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 4 LO
 #bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 5 LO
 #bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 6 LO
-bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 7 LO
-bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 8 LO
-bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 9 LO
-bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 10 LO
+#bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 7 LO
+#bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 8 LO
+#bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 9 LO
+#bsub -q 8nh -u pippo1234 run.sh mc_all 25ns 2015D 10 LO
 
